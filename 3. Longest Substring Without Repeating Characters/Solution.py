19601a3e1b7e9e96f381a3f5370c9b3ee76da41c
furutuki <furutuki@foxmail.com>
2019-02-14 16:39:58 +0800

    优化代码成单层循环

--- 3. Longest Substring Without Repeating Characters/Solution.py
+++ 3. Longest Substring Without Repeating Characters/Solution.py
@@ -2,16 +2,21 @@ class Solution:
     def lengthOfLongestSubstring(self, s: 'str') -> 'int':
         max_count = 0
         my_set = set()
-        for startIndex in range(0, len(s)):
-            for index in range(startIndex, len(s)):
-                if s[index] in my_set:
-                    if len(my_set) > max_count:
-                        max_count = len(my_set)
-                    del my_set
-                    my_set = set()
-                    break
-                else:
-                    my_set.add(s[index])
+        start_index = 0
+        tail_index = 0
+        while start_index < len(s) and tail_index < len(s):
+            if s[tail_index] in my_set:
+                if len(my_set) > max_count:
+                    max_count = len(my_set)
+
+                pos = s[start_index:].find(s[tail_index]) + start_index
+                del my_set
+                my_set = set(list(s[pos + 1:tail_index + 1]))
+                start_index = pos + 1
+            else:
+                my_set.add(s[tail_index])
+
+            tail_index += 1
 
         if len(my_set) > 0 and len(my_set) > max_count:
             max_count = len(my_set)
@@ -20,7 +25,8 @@ class Solution:
 
 
 s = Solution()
-print(s.lengthOfLongestSubstring("abcabcbb"))
+print(s.lengthOfLongestSubstring("dvdf"))
 print(s.lengthOfLongestSubstring("bbbbb"))
+print(s.lengthOfLongestSubstring("abcabcbb"))
 print(s.lengthOfLongestSubstring("pwwkew"))
 print(s.lengthOfLongestSubstring(" "))
