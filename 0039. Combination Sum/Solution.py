4b63dda7d047f5ea2e4a1fe635e76c9ff72ffcb6
furutuki <furutuki@foxmail.com>
2019-11-04 23:56:45 +0800

    problem 0039. Combination Sum
    回溯

---
+++ 0039. Combination Sum/Solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+
+    def back_track(self, candidates: List[int], res: List[int], cur_res: List[int],  cur_sum: int, index: int, target: int):
+        if cur_sum > target:
+            return
+        elif cur_sum == target:
+            res.append(cur_res)
+            return
+        else:
+            for i in range(index, len(candidates)):
+                self.back_track(candidates, res, cur_res + [candidates[i]], cur_sum + candidates[i], i, target)
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+        self.back_track(candidates, res, [], 0, 0, target)
+        return res
+
+
+s = Solution()
+print(s.combinationSum([2,3,5], 8))
