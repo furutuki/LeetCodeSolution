d18055cc1bdfa0f7207d4b9c519953ca3c5f7872
furutuki <furutuki@foxmail.com>
2019-11-05 00:02:02 +0800

    代码优化

--- 0039. Combination Sum/Solution.py
+++ 0039. Combination Sum/Solution.py
@@ -4,13 +4,13 @@ from typing import List
 class Solution:
 
     def back_track(self, candidates: List[int], res: List[int], cur_res: List[int],  cur_sum: int, index: int, target: int):
-        if cur_sum > target:
-            return
-        elif cur_sum == target:
+        if cur_sum == target:
             res.append(cur_res)
             return
         else:
             for i in range(index, len(candidates)):
+                if cur_sum + candidates[i] > target:
+                    break
                 self.back_track(candidates, res, cur_res + [candidates[i]], cur_sum + candidates[i], i, target)
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
