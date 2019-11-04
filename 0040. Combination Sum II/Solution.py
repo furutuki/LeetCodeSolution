08fc7f55fc48182e6c1fa2017b28c503b438f6b6
furutuki <furutuki@foxmail.com>
2019-11-05 00:10:13 +0800

    problem 0040. Combination Sum II

--- 0040. Combination Sum II/Solution.py
+++ 0040. Combination Sum II/Solution.py
@@ -1,2 +1,25 @@
+from typing import List
+
+
 class Solution:
+
+    def back_track(self, candidates: List[int], res: List[int], cur_res: List[int],  cur_sum: int, index: int, target: int):
+        if cur_sum == target:
+            if cur_res not in res:
+                res.append(cur_res)
+            return
+        else:
+            for i in range(index, len(candidates)):
+                if cur_sum + candidates[i] > target:
+                    break
+                self.back_track(candidates, res, cur_res + [candidates[i]], cur_sum + candidates[i], i + 1, target)
+
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+        self.back_track(candidates, res, [], 0, 0, target)
+        return res
+
+
+s = Solution()
+print(s.combinationSum2([10,1,2,7,6,1,5], 8))
