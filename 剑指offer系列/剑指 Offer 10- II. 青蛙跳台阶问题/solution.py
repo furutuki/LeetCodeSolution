926e3c17a8818647faacc6a76fdff3288421f9d6
furutuki <furutuki@foxmail.com>
2020-07-20 23:46:26 +0800

    青蛙跳台阶

---
+++ 剑指offer系列/剑指 Offer 10- II. 青蛙跳台阶问题/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numWays(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        f = [0 for i in range(n + 1)]
+        f[0] = 0
+        f[1] = 1
+        f[2] = 2
+        for i in range(3, n + 1):
+            f[i] = f[i - 1] + f[i - 2]
+
+        return f[n] % 1000000007
