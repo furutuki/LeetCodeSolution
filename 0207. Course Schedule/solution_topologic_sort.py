a4b1692c87803f026fe4c0b34133bdc119a63fd7
furutuki <furutuki@foxmail.com>
2020-08-04 16:29:07 +0800

    优化代码

--- 0207. Course Schedule/solution_topologic_sort.py
+++ 0207. Course Schedule/solution_topologic_sort.py
@@ -3,13 +3,15 @@ from typing import List
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        """顶点的入度"""
         d = [0 for _ in range(numCourses)]
+
+        """入度为0的顶点集合"""
         s = []
+
+        """计算所有顶点的入度"""
         for edge in prerequisites:
-            """从m到n有一条边，n的入度+1"""
-            m = edge[0]
-            n = edge[1]
-            d[m] += 1
+            d[edge[0]] += 1
 
         """记录一下入度为0的顶点集合"""
         for i in range(numCourses):
@@ -23,10 +25,12 @@ class Solution:
             popped_v_cnt += 1
 
             """从v出发的终点的入度-1"""
-            for edge in prerequisites:
+            for i in range(len(prerequisites) - 1, -1, -1):
+                edge = prerequisites[i]
                 if edge[1] == v:
                     d[edge[0]] -= 1
                     if d[edge[0]] == 0:
                         s.append(edge[0])
+                    del prerequisites[i]
 
         return False if popped_v_cnt < numCourses else True
