ba979bcdce2899daf7cf8a213033dd82bec358d6
furutuki <furutuki@foxmail.com>
2020-09-08 14:05:40 +0800

    剪枝优化

---
+++ 0077. Combinations/solution.backtrace2.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+
+    def backtrace(self, nums: List[int], k: int,  step: int, res: List[int], ans: List[int]):
+        if len(res) == k:
+            ans.append(res[:])
+            return
+
+        for i in range(step, len(nums) - (k - len(res)) + 1):
+            res.append(nums[i])
+            self.backtrace(nums, k, i + 1, res, ans)
+            res.pop()
+
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        if n == 0 or k == 0:
+            return ans
+
+        nums = [i for i in range(n + 1)]
+        self.backtrace(nums, k, 1, [], ans)
+        return ans
+
+s = Solution()
+print(s.combine(4,2))
