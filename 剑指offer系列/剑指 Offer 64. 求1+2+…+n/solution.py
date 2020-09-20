d257d6ef9e786f6c0e7498c79b570fd712441077
furutuki <furutuki@foxmail.com>
2020-09-20 18:39:21 +0800

    短路路径求1+2+...+n

---
+++ 剑指offer系列/剑指 Offer 64. 求1+2+…+n/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def __init__(self):
+        self.res = 0
+
+    def sumNums(self, n: int) -> int:
+        n > 1 and self.sumNums(n - 1)
+        self.res += n
+        return self.res
+
+
+s = Solution()
+print(s.sumNums(3))
