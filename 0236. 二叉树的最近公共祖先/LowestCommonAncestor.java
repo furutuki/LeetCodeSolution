2055dc15e4b31391bde856c8d0babdb89706902b
furutuki <furutuki@foxmail.com>
2020-11-16 22:58:18 +0800

    LCA问题（最近公共祖先）

---
+++ 0236. 二叉树的最近公共祖先/LowestCommonAncestor.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode leftResult = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightResult = lowestCommonAncestor(root.right, p, q);
+        if (leftResult == null) {
+            return rightResult;
+        }
+        if (rightResult == null) {
+            return leftResult;
+        }
+        return root;
+    }
+}
