33a40103e7b410098406eb20a05c5984aa608d4a
furutuki <furutuki@foxmail.com>
2020-11-19 14:29:53 +0800

    移动0

---
+++ 0283. 移动零/RemoveDuplicates.java
@@ -0,0 +1,15 @@
+class RemovefDuplicates {
+    public void moveZeroes(int[] nums) {
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        while (slow < nums.length) {
+            nums[slow++] = 0;
+        }
+    }
+}
